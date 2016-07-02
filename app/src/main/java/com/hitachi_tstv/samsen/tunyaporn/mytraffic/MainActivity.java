5f49ea5290a11348ae36279607da834de52727c8
evijung <evides01@gmail.com>
2016-07-02 15:00:05 +0700

    Finish 2/07/2016

--- app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/mytraffic/MainActivity.java
+++ app/src/main/java/com/hitachi_tstv/samsen/tunyaporn/mytraffic/MainActivity.java
@@ -1,13 +1,46 @@
 package com.hitachi_tstv.samsen.tunyaporn.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
+//Main Class
 public class MainActivity extends AppCompatActivity {
 
+    //Explicit
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
     @Override
+    //Main Method
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+        //Controller for Button
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //onClick
+            public void onClick(View v) {
+                //Sound Effect
+                MediaPlayer mediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.lamp);
+                mediaPlayer.start();
+
+                //Web View
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://youtu.be/HEI1WsqFxhY"));
+                startActivity(intent);
+            }
+        });
+
     }
 }
